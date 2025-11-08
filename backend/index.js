638fdb05995e4934dc671fd038af77f87d577c01
FranR <francisca.rabanal2201@alumnos.ubiobio.cl>
2025-11-07 21:12:07 -0300

    prueba de crud denuncia

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import { probarConexion } from "./src/config/db.js";
-import routes from "./src/routes/index.routes.js";       
-
+import routes from "./src/routes/index.routes.js";
 
 dotenv.config(); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const FRONTEND_URL = process.env.VITE_API_URL || "http://localhost:5173";
+const PORT = process.env.PORT  || 3000;
+const FRONTEND_URL = process.env.VITE_API_URL || "http://localhost:5173/";
 
 // Middlewares globales
 app.use(cors({
@@ -27,26 +25,8 @@ app.get("/", (req, res) => res.send("Servidor backend operativo 🚀"));
 // Monta las rutas principales bajo el prefijo /api
 app.use("/api", routes);
 
-// Middleware de manejo de errores global
-app.use((err, req, res, next) => {
-  console.error("❌ Error:", err);
-
-  const status = err.status || 500;
-  res.status(status).json({
-    ok: false,
-    message: err.message || "Error interno del servidor",
-    // si lanzas errores con "err.details", los muestra también
-    ...(err.details ? { details: err.details } : {}),
-  });
-});
-
-
-
-// 🧪 Prueba de conexión a BD (opcional)
-probarConexion();
-
 //  inicio del servidor
 app.listen(PORT, () => {
-  console.log(`✅ Servidor escuchando en http://localhost:${PORT}`);
-  console.log(`🌐 CORS habilitado para: ${FRONTEND_URL}`);
+  console.log("✅ Servidor escuchando en http://localhost:${PORT}");
+  console.log("🌐 CORS habilitado para: ${FRONTEND_URL}");
 });
